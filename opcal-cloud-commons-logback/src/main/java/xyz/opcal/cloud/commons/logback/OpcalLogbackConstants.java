e477fb32d6cd0b8545fae77952155a035528b353
gissily <gissily@outlook.com>
2021-08-05 15:21:27 +0800

    fix copyright and javadoc

--- opcal-cloud-commons-logback/src/main/java/xyz/opcal/cloud/commons/logback/OpcalLogbackConstants.java
+++ opcal-cloud-commons-logback/src/main/java/xyz/opcal/cloud/commons/logback/OpcalLogbackConstants.java
@@ -1,11 +1,37 @@
+/*
+ * Copyright 2021 Opcal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xyz.opcal.cloud.commons.logback;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+/**
+ * @since 0.1.0
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OpcalLogbackConstants {
 
-	public static final String MDC_THREAD_ID = "mdcThreadId";
-	public static final String CURRENT_THREAD_ID = "currentThreadId";
+    /**
+     * Key name for custom's thread id in MDC
+     */
+    public static final String MDC_THREAD_ID = "mdcThreadId";
+
+    /**
+     * Variable name for logback pattern
+     */
+    public static final String CURRENT_THREAD_ID = "currentThreadId";
 }
--- opcal-cloud-commons-logback/src/main/java/xyz/opcal/cloud/commons/logback/OpcalPatternLayoutEncoder.java
+++ opcal-cloud-commons-logback/src/main/java/xyz/opcal/cloud/commons/logback/OpcalPatternLayoutEncoder.java
@@ -1,28 +1,57 @@
+/*
+ * Copyright 2021 Opcal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xyz.opcal.cloud.commons.logback;
 
 import org.springframework.boot.logging.logback.ColorConverter;
 import org.springframework.boot.logging.logback.ExtendedWhitespaceThrowableProxyConverter;
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter;
-
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.pattern.PatternLayoutEncoderBase;
 
+/**
+ * A logback pattern layout encoder which will convert custom thread id to the pattern
+ * and integrating with Spring's logback converters.
+ * Usage in logback.xml
+ * <pre>{@code
+ * 	<appender name="CONSOLE" class="ch.qos.logback.core.ConsoleAppender">
+ *      <encoder class="xyz.opcal.cloud.commons.logback.OpcalPatternLayoutEncoder">
+ *          <pattern>%clr(%d{${LOG_DATEFORMAT_PATTERN:-yyyy-MM-dd HH:mm:ss.SSS}}){faint} %clr(${LOG_LEVEL_PATTERN:-%5p}) %clr(${PID:- }){yellow} %clr(---){faint} %clr([%15.15t - %currentThreadId]){magenta} %clr(%-40.40logger{39}){cyan} %clr(:){faint} %m%n${LOG_EXCEPTION_CONVERSION_WORD:-%wEx}</pattern>
+ *      </encoder>
+ *  </appender>
+ * }</pre>
+ *
+ * @since 0.1.0
+ */
 public class OpcalPatternLayoutEncoder extends PatternLayoutEncoderBase<ILoggingEvent> {
 
-	@Override
-	public void start() {
-		PatternLayout.defaultConverterMap.put("clr", ColorConverter.class.getName());
-		PatternLayout.defaultConverterMap.put("wex", WhitespaceThrowableProxyConverter.class.getName());
-		PatternLayout.defaultConverterMap.put("wEx", ExtendedWhitespaceThrowableProxyConverter.class.getName());
-		PatternLayout.defaultConverterMap.put(OpcalLogbackConstants.CURRENT_THREAD_ID, OpcalThreadClassicConverter.class.getName());
-		PatternLayout patternLayout = new PatternLayout();
-		patternLayout.setContext(context);
-		patternLayout.setPattern(getPattern());
-		patternLayout.setOutputPatternAsHeader(outputPatternAsHeader);
-		patternLayout.start();
-		this.layout = patternLayout;
-		super.start();
-	}
+    @Override
+    public void start() {
+        PatternLayout.defaultConverterMap.put("clr", ColorConverter.class.getName());
+        PatternLayout.defaultConverterMap.put("wex", WhitespaceThrowableProxyConverter.class.getName());
+        PatternLayout.defaultConverterMap.put("wEx", ExtendedWhitespaceThrowableProxyConverter.class.getName());
+        PatternLayout.defaultConverterMap.put(OpcalLogbackConstants.CURRENT_THREAD_ID, OpcalThreadClassicConverter.class.getName());
+        PatternLayout patternLayout = new PatternLayout();
+        patternLayout.setContext(context);
+        patternLayout.setPattern(getPattern());
+        patternLayout.setOutputPatternAsHeader(outputPatternAsHeader);
+        patternLayout.start();
+        this.layout = patternLayout;
+        super.start();
+    }
 
 }
