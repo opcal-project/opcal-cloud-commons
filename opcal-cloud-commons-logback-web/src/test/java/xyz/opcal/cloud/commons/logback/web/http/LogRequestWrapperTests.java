7de2d6dfbc69a98e70924b8a70939a2fded7b2f8
gissily <gissily@outlook.com>
2022-01-15 18:24:32 +0800

    fix test case

---
+++ opcal-cloud-commons-logback-web/src/test/java/xyz/opcal/cloud/commons/logback/web/http/LogRequestWrapperTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 Opcal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.opcal.cloud.commons.logback.web.http;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.ServletInputStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class LogRequestWrapperTests {
+
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void test() throws JsonProcessingException {
+		Map<String, Object> content = new HashMap<>();
+		content.put("name", "apple");
+		content.put("quantity", 10);
+
+		MockHttpServletRequest request = new MockHttpServletRequest(HttpMethod.POST.name(), "/api/product?ca=123");
+		request.setServletPath("/api");
+		request.setPathInfo("/product");
+		request.setQueryString("ca=123");
+		request.setParameter("ca", "123");
+		request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		request.setContent(objectMapper.writeValueAsBytes(content));
+
+		LogRequestWrapper requestWrapper = new LogRequestWrapper(UUID.randomUUID().toString(), request, objectMapper.writeValueAsString(content));
+		try (ServletInputStream inputStream = requestWrapper.getInputStream()) {
+			inputStream.setReadListener(null);
+			assertTrue(inputStream.isReady());
+			assertFalse(inputStream.isFinished());
+		} catch (IOException e) {
+			log.error("request error", e);
+		}
+	}
+}
---
+++ opcal-cloud-commons-logback-web/src/test/java/xyz/opcal/cloud/commons/logback/web/http/LogResponseWrapperTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Opcal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.opcal.cloud.commons.logback.web.http;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.ServletOutputStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class LogResponseWrapperTests {
+
+	@Test
+	void test() {
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		LogResponseWrapper responseWrapper = new LogResponseWrapper(UUID.randomUUID().toString(), response);
+		try (ServletOutputStream outputStream = responseWrapper.getOutputStream()) {
+			outputStream.setWriteListener(null);
+			assertTrue(outputStream.isReady());
+		} catch (IOException e) {
+			log.error("response error: ", e);
+		}
+	}
+}
