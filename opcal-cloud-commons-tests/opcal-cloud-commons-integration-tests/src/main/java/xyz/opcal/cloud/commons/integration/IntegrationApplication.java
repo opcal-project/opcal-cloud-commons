6cfb83e5f963836aa77306e36e90d7d5f441f0f3
gissily <gissily@outlook.com>
2022-05-30 17:07:51 +0800

    fix test

--- opcal-cloud-commons-tests/opcal-cloud-commons-integration-tests/src/main/java/xyz/opcal/cloud/commons/integration/IntegrationApplication.java
+++ opcal-cloud-commons-tests/opcal-cloud-commons-integration-tests/src/main/java/xyz/opcal/cloud/commons/integration/IntegrationApplication.java
@@ -19,6 +19,7 @@ package xyz.opcal.cloud.commons.integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class IntegrationApplication {
 
