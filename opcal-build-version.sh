f8bb91275e23e2a5ffbffff73324d01b7f3fe668
gissily <gissily@outlook.com>
2022-08-05 14:06:59 +0800

    update opcal-build-version.sh script

--- opcal-build-version.sh
+++ opcal-build-version.sh
@@ -6,7 +6,15 @@ DIR_PATH=`dirname ${SCRIPT}`
 
 VERSION=$1
 
-echo ${VERSION}
-
+echo "opcal build version is [${VERSION}]"
+# update xyz.opcal.build version
 xmlstarlet edit -P -L -O -u "/_:project/_:parent/_:version" -v ${VERSION} ${DIR_PATH}/pom.xml
 xmlstarlet edit -P -L -O -u "/_:project/_:parent/_:version" -v ${VERSION} ${DIR_PATH}/opcal-cloud-commons-dependencies/pom.xml
+
+# get spring boot version from opcal build project
+SPRING_BOOT_VERSION=$(${DIR_PATH}/mvnw help:evaluate -Dexpression=spring-boot.version | grep "^[^\\[]" |grep -v Download)
+
+echo "spring boot version is [${SPRING_BOOT_VERSION}]"
+
+# spring boot parent version
+xmlstarlet edit -P -L -O -u "/_:project/_:parent/_:version" -v ${SPRING_BOOT_VERSION} ${DIR_PATH}/opcal-cloud-starter-parent/pom.xml
