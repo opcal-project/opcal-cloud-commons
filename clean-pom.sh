b45efbda0719bf06ac84f8f0e0176e976a9c884a
gissily <gissily@outlook.com>
2022-11-26 17:41:48 +0800

    fix ci script

--- clean-pom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT=`readlink -f "${BASH_SOURCE:-$0}"`
-DIR_PATH=`dirname ${SCRIPT}`
-
-find ${DIR_PATH}/ -type f -iname '.flattened-pom.xml' | xargs -I {} rm -f {};
-find ${DIR_PATH}/ -type f -iname 'pom.xml.versionsBackup' | xargs -I {} rm -f {};
